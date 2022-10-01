1ee8cf1435e131d55268d205aa91110ea8958bc3
Alan <44586947+AliOlixx@users.noreply.github.com>
2022-10-01 14:35:44 +0200

    Navbar añadida al registro

--- app/register.php
+++ app/register.php
@@ -35,31 +35,50 @@ if(isset($_SESSION['user'])){
 
 }else{
 ?>
-    <form action="register.php" method="POST">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" value=<?php echo "{$datos['username']}"; ?>><br>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Muffin</title>
+        <link rel="icon" href="/images/muffin.ico">
 
-        <!--
-            TODO: CUIDADO -> El input text se lleva mal con los espacios del nombre_apellidos
-        --> 
-        <label for="nombre_apellidos">Nombre y apellidos:</label><br>
-        <input type="text" id="nombre_apellidos" name="nombre_apellidos" value=""><br>
+        <!-- Incluimos los estilos necesarios -->
+        <link rel="stylesheet" href="/styles/common.css?version=0.1">
+        <link rel="stylesheet" href="/styles/nav_bar.css?version=0.1">
 
-        <label for="DNI">DNI:</label><br>
-        <input type="text" id="DNI" name="DNI" value=""><br>
+        <!-- Incluimos unas fuentes online -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
 
-        <label for="telf">Teléfono:</label><br>
-        <input type="text" id="telf" name="telf" value=""><br>
+    </head>
+    <body>
+        <!-- Incluimos la barra del menú -->
+        <?php require_once("components/nav_bar.php")?>
 
-        <label for="email">email:</label><br>
-        <input type="text" id="email" name="email" value=""><br><br>
+        <form action="register.php" method="POST">
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" value=<?php echo "{$datos['username']}"; ?>><br>
 
-        <label for="password">New password:</label><br>
-        <input type="password" id="password" name="password" value=""><br><br>
-        <label for="password">Repeat password:</label><br>
-        <input type="password" id="password2" name="password2" value=""><br><br>
+            <label for="nombre_apellidos">Nombre y apellidos:</label><br>
+            <input type="text" id="nombre_apellidos" name="nombre_apellidos" value=""><br>
 
-        <input type="submit" name="register" value="Register">
-    </form>
+            <label for="DNI">DNI:</label><br>
+            <input type="text" id="DNI" name="DNI" value=""><br>
 
+            <label for="telf">Teléfono:</label><br>
+            <input type="text" id="telf" name="telf" value=""><br>
+
+            <label for="email">email:</label><br>
+            <input type="text" id="email" name="email" value=""><br><br>
+
+            <label for="password">New password:</label><br>
+            <input type="password" id="password" name="password" value=""><br><br>
+            <label for="password">Repeat password:</label><br>
+            <input type="password" id="password2" name="password2" value=""><br><br>
+
+            <input type="submit" name="register" value="Register">
+        </form>
+
+    </body>
+</html>
 <?php } ?>
