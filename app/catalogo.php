46d2bdacb7351f8e246d5c2a0ade2767e0aa1ea2
Alan <44586947+AliOlixx@users.noreply.github.com>
2022-11-12 19:20:14 +0100

    Problemas solucionados

--- app/catalogo.php
+++ app/catalogo.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-header('Content-Type: text/html; charset=utf-8');
-header("Content-Security-Policy: default-src 'self'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com; connect-src 'self' https://ka-f.fontawesome.com");
+header('content-type: text/html; charset=utf-8');
+header("content-security-policy: default-src 'self'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
 
 if (empty($_SESSION['_token'])) {
     if (function_exists('mcrypt_create_iv')) {
