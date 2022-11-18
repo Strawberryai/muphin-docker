4b155f426cb2b71f5ed5f9a09c7c4726f36e61ae
Adrian <adrianlopezoyon@gmail.com>
2022-11-18 20:57:26 +0100

    XPoweredBy PHP

--- app/log_out.php
+++ app/log_out.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+header("X-Frame-Options: DENY");
 header('Content-Type: text/html; charset=utf-8');
 header("Content-Security-Policy: default-src 'self';frame-ancestors 'none'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com; connect-src 'self' https://ka-f.fontawesome.com");
 
--- app/perfil.php
+++ app/perfil.php
@@ -1,6 +1,7 @@
 <?php
 // Pagina para modificar los datos del usuario
 session_start();
+header("X-Frame-Options: DENY");
 header('Content-Type: text/html; charset=utf-8');
 header("Content-Security-Policy: default-src 'self';frame-ancestors 'none'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
 
