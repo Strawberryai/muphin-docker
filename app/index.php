838a8a631f87bd9be0657cf18365e64f70236b38
Adrian <adrianlopezoyon@gmail.com>
2022-11-18 19:46:01 +0100

    Cambio Anticlickjacking

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 header('Content-Type: text/html; charset=utf-8');
-header("Content-Security-Policy: default-src 'self'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
+header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none';font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
 ?>
 
 <!-- Entrada principal de la página web -->
--- app/log_in.php
+++ app/log_in.php
@@ -3,7 +3,7 @@ session_start();
 require 'Database.php';
 require 'Logs.php';
 header('Content-Type: text/html; charset=utf-8');
-header("Content-Security-Policy: default-src 'self'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
+header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none';font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
 session_start();
 
 if (empty($_SESSION['_token'])) {
