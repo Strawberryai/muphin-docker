6536d54113a460e45c8f74db7300a987be3871b8
Adrian <adrianlopezoyon@gmail.com>
2022-11-18 20:32:03 +0100

    HttpOnly Flag

--- app/register.php
+++ app/register.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+header('setcookie("sessionid", "QmFieWxvbiA1", ['httponly' => true]');
 header('Content-Type: text/html; charset=utf-8');
 header("Content-Security-Policy: default-src 'self';frame-ancestors 'none'; font-src fonts.gstatic.com https://ka-f.fontawesome.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com 'unsafe-inline'; connect-src 'self' https://ka-f.fontawesome.com");
 require('Logs.php');
